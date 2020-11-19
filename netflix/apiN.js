85439381d0e2d66d0213e1787e0f0ce31e4366d5
KMx404 <mohamedelfaleh5@gmail.com>
2020-11-19 11:09:52 +0100

    :star: adding the TMDB api's

---
+++ netflix/apiN.js
@@ -0,0 +1,3 @@
+// v3 =  6a39ba045a0ac8616c52c2e4ab809f6a
+// example api request https://api.themoviedb.org/3/movie/550?api_key=6a39ba045a0ac8616c52c2e4ab809f6a
+// api read access token = eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YTM5YmEwNDVhMGFjODYxNmM1MmMyZTRhYjgwOWY2YSIsInN1YiI6IjVmYjYzNTY2OWFlNjEzMDAzZmE0YzU3NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jdkoREP9Bjb46RYe3vNd-lxuZp2bEDcsCjDK7SN-Jm0
